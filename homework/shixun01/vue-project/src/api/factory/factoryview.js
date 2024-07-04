deee287a969183861d9f0f64c241bcb437b111d4
nothingyuancando <1579035512@qq.com>
2024-07-04 17:28:44 +0800

    add shixun pro

--- homework/shixun01/vue-project/src/api/factory/factoryview.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-import request from '@/utils/request.js';
-
-export const getFactoryOverviewData = () => {
-    return request.get('/factory/view');
-};
--- homework/shixun01/vue-project/src/api/factory/getreport.js
+++
@@ -1,7 +0,0 @@
-import request from '@/utils/request.js';
-
-// 获取报工记录列表
-export const getWorkReportList = () => {
-    return request.get('/factory/order');
-};
-
